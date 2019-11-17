42c2da0f02e8d521b20f3177558182a3e194abd2
Shakhawat Hossain <shakhawat.sk@gmail.com>
2019-11-17 15:16:53 +0600

    Laravel 6.0 LTS Multiauth

---
+++ app/Http/Controllers/editor/dashboardController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\editor;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class dashboardController extends Controller
+{
+    public function index(){
+        echo "Editor Dashboard";
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['as'=>'admin.','prefix'=>'admin','namespace'=>'admin','middleware'=>['auth','admin']], function(){
+	Route::get('dashboard','dashboardController@index')->name('dashboard');
+});
+
+Route::group(['as'=>'editor.','prefix'=>'editor','namespace'=>'editor','middleware'=>['auth','editor']], function(){
+	Route::get('dashboard','dashboardController@index')->name('dashboard');
+});
+Route::group(['as'=>'user.','prefix'=>'user','namespace'=>'user','middleware'=>['auth','user']], function(){
+	Route::get('dashboard','dashboardController@index')->name('dashboard');
+});
